c7e68b60db0aace522351c9270b312b438b4c5be
luuxis <61792917+luuxis@users.noreply.github.com>
2021-04-08 07:15:46 +0200

    login error

--- app/assets/js/panels/login.js
+++ app/assets/js/panels/login.js
@@ -1,24 +1,38 @@
 'use strict';
+
 const { auth } = require("./assets/js/lib/utils.js");
 
 class Login {
   static id = "login";
 
   async init(){
-    this.Login();
+   this.changePanel("home");
     if (auth.isLogged()){
       this.changePanel("home");
-
     }
   }
 
-
-  Login(){
-
-
+  function login(online) {
+    if (online){
+      if (document.querySelector(".pseudo").value == ""){
+        document.querySelector(".error").style.display = "block";
+        return;
+      }
+    }
+    document.querySelector(".pseudo").disabled = true;
+    document.querySelector(".password").disabled = true;
+    document.querySelector(".error").style.display = "none";
+    auth.Login(document.querySelector(".pseudo").value, document.querySelector(".password").value).then(user => {
+      this.changePanel("home");
+    }).catch (err => {
+      document.querySelector(".pseudo").disabled = false;
+      document.querySelector(".password").disabled = false;
+      document.querySelector(".error").style.display = "block";
+    })
   }
 
 
-
 }
+
+
 export default Login;
