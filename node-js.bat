6d541477b76a6ec74f6bfe2753ffb8f36043086c
luuxis <61792917+luuxis@users.noreply.github.com>
2021-04-30 01:46:35 +0200

    add install gulp to node-js portable

--- node-js.bat
+++ node-js.bat
@@ -9,10 +9,13 @@ if not exist "%CD%\node-js\node.exe" (
     powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://luuxis.legtux.org/res/craftdium/7z.exe' '%CD%\node-js\7z.exe'"
     powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://luuxis.legtux.org/res/craftdium/7z.dll' '%CD%\node-js\7z.dll'"
     powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://luuxis.legtux.org/dev/node-js.7z' '%CD%\node-js\node-js.7z'"
+    powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://luuxis.legtux.org/dev/gulp.7z' '%CD%\node-js\gulp.7z"
     7z x -o%CD%\node-js %CD%\node-js\node-js.7z
+    7z x -o%CD%\node-js %CD%\node-js\gulp.7z
     del %CD%\node-js\7z.exe
     del %CD%\node-js\7z.dll
     del %CD%\node-js\node-js.7z
+    del %CD%\node-js\gulp.7z
 )
 cls
 cmd
