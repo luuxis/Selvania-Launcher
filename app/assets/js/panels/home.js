f520d61f87432b2f1d8355ad55cc5488e3e6f336
luuxis <61792917+luuxis@users.noreply.github.com>
2021-05-09 10:39:33 +0200

    fix log launcher

--- app/assets/js/panels/home.js
+++ app/assets/js/panels/home.js
@@ -1,24 +1,37 @@
 const { config, auth } = require('./assets/js/utils.js');
 const { MCAuth, MCLaunch } = require('emc-core-luuxis');
 const launcher = new MCLaunch;
-const dataDirectory = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Application Support' : process.env.HOME)
 
-config.fetch().then(res => {
+function play(){
+    config.fetch().then(res => {
+        if (auth.isLogged()){
+            document.querySelector(".play-btn").disabled = true;
 
-let opts = {
-    url: res.game_url,
-    overrides: {
-        detached: false
-    },
-    authorization: MCAuth.auth("username", ""),
-    root: dataDirectory + "/" + res.dataDirectory,
-    version: res.game_version,
-    forge: res.game_version + "-forge-" + res.forge_version,
-    memory: {
-        max: "1G",
-        min: "1G"
-    }
+
+                let opts = {
+                    url: res.game_url,
+                    overrides: {
+                        detached: false
+                    },
+                    authorization: MCAuth.auth("username", ""),
+                    root: dataDirectory + "/" + res.dataDirectory,
+                    version: res.game_version,
+                    forge: res.game_version + "-forge-" + res.forge_version,
+                    memory: {
+                        max: "1G",
+                        min: "1G"
+                    }
+                }
+                
+                
+                launcher.launch(opts);
+                launcher.on('debug', (e) => console.log("[DEBUG]" + e));
+                launcher.on('data', (e) => console.log("[DATA]" + e));
+            launcher.on('error', (e) => {
+                console.log("[ERROR]" + e)
+            });
+        } else {
+            window.location.href = "../launcher.html"
+        }
+    })
 }
-
-launcher.launch(opts);
-})
