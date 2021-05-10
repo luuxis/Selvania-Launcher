382cc67b806cbf56adb125da29a7c682a8a12827
luuxis <61792917+luuxis@users.noreply.github.com>
2021-05-10 14:51:11 +0200

    Update frame.js

--- app/assets/js/panels/frame.js
+++ app/assets/js/panels/frame.js
@@ -2,9 +2,6 @@ let win = nw.Window.get();
 window.isDev = (window.navigator.plugins.namedItem('Native Client') !== null);
 
 if (process.platform == "win32") {
-    document.querySelector(".frame").style.display = "none";
-    const div = document.querySelector('frame');
-    div.setAttribute('style', ' display: none');
 
 }
 
