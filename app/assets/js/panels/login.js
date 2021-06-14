722945ef325ac7f86c3ffbda8cbef842468674f2
andre <61792917+luuxis@users.noreply.github.com>
2021-06-14 18:48:41 +0200

    fix login 30%

--- app/assets/js/panels/login.js
+++ app/assets/js/panels/login.js
@@ -1,4 +1,22 @@
 const { config, auth } = require('./assets/js/utils.js');
+const fs = require("fs")
+
+
+function setloging(){
+  const patch =("./data")
+  const login = (patch + "/login.json" )
+
+  const name = document.querySelector(".pseudo").value;
+  const password = document.querySelector(".password").value;
+
+  let data ={ 
+    "user": name,
+    "password": password,
+  }; 
+  const dataStringified = JSON.stringify(data);
+  fs.mkdirSync(patch)
+  fs.writeFileSync(login, dataStringified);
+}
 
 function login(online) {
     if (online){
@@ -15,6 +33,7 @@ function login(online) {
     document.querySelector(".password").disabled = true;
     document.querySelector(".error").style.display = "none";
     auth.login(document.querySelector(".pseudo").value, document.querySelector(".password").value).then(user => {
+      setloging()
       window.location.href = "./home.html";
     }).catch (err => {
       document.querySelector(".pseudo").disabled = false;
