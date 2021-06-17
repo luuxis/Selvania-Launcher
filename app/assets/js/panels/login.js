b700eee42c2ee0e0bcac1b42a364f5d50d64b732
luuxis <61792917+luuxis@users.noreply.github.com>
2021-06-17 17:57:50 +0200

    fix

--- app/assets/js/panels/login.js
+++ app/assets/js/panels/login.js
@@ -31,7 +31,6 @@ function setlogging(){
   })
 }
 
-
 function login() {
     if (document.querySelector(".pseudo").value == "" || document.querySelector(".password").value == ""){
       document.querySelector(".error").style.display = "block";
