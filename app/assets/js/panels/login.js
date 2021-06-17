0bdaae527a6c8232b50b38ff58c6a3031b639b7c
luuxis <61792917+luuxis@users.noreply.github.com>
2021-06-17 15:42:45 +0200

    fix crack

--- app/assets/js/panels/login.js
+++ app/assets/js/panels/login.js
@@ -30,13 +30,24 @@ function setlogging(){
 }
 
 
-function login(online) {
-    if (online){
-      if (document.querySelector(".password").value == ""){
-        document.querySelector(".error").style.display = "block";
-        return;
-      }
+function login() {
+    if (document.querySelector(".pseudo").value == "" || document.querySelector(".password").value == ""){
+      document.querySelector(".error").style.display = "block";
+      return;
     }
+    document.querySelector(".pseudo").disabled = true;
+    document.querySelector(".password").disabled = true;
+    document.querySelector(".error").style.display = "none";
+    auth.login(document.querySelector(".pseudo").value, document.querySelector(".password").value).then(user => {
+      setlogging()
+    }).catch (err => {
+      document.querySelector(".pseudo").disabled = false;
+      document.querySelector(".password").disabled = false;
+      document.querySelector(".error").style.display = "block";
+    })
+  }
+
+  function loginoff() {
     if (document.querySelector(".pseudo").value == ""){
       document.querySelector(".error").style.display = "block";
       return;
