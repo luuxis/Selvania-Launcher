a76b5476cdd234dfe99d2b207a30621eb77bcb97
luuxis <61792917+luuxis@users.noreply.github.com>
2021-06-17 02:25:20 +0200

    added automatic connection to the launcher

--- app/assets/js/launcher.js
+++ app/assets/js/launcher.js
@@ -1,22 +1,27 @@
-const { config, auth } = require('./assets/js/utils.js');
+const { config } = require('./assets/js/utils.js');
+const fs = require("fs")
+const { MCAuth } = require('emc-core-luuxis');
 const dataDirectory = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Application Support' : process.env.HOME)
 
-config.config().then(res => {
-  const patch = (dataDirectory + "/" + config.dataDirectory + "/login.json") 
-  if (fs.existsSync(patch)){
-    const login = require(patch)
-    auth.login(login.user, login.password).then(user => {
-      window.location.href = "./home.html";
-    })
-  }
-})
 
-config.isonline().then(online => {
-    if (online) {
+config.config().then(config => {
+  if(fs.existsSync(dataDirectory + "/" + config.dataDirectory + "/login.json")) {
+    let rawData = fs.readFileSync(dataDirectory + "/" + ".arche" + "/login.json")
+    let json = JSON.parse(rawData);
+    MCAuth.auth(json.user, json.password).then(user => {
+      window.location.href = "./panels/home.html";
+    }).catch (err => {
+      
+    })
+  } else {
+    config.isonline().then(online => {
+      if (online) {
         console.log("Loading online login \(officiel login\)");
         window.location.href = "./panels/login-online.html"
-    } else {
+      } else {
         console.log("Loading offline login \(crack login\)");
         window.location.href = "./panels/login-offline.html"
-    }
+      }
+    })
+  }
 })
