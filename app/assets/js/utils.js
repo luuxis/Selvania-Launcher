92f7fb8ce9282cca47525b279dbedc370f650c25
andre <61792917+luuxis@users.noreply.github.com>
2021-06-30 01:06:47 +0200

    fix

--- app/assets/js/utils.js
+++ app/assets/js/utils.js
@@ -2,6 +2,5 @@ module.exports = {
     config: require("./utils/config.js"),
     auth: require("./utils/auth.js"),
     status_server: require("./utils/status_server/lib/index.js"),
-    crypt: require("./utils/crypto.js"),
     microsoft: require("./utils/mincrosoft/microsoft.js")
 }
