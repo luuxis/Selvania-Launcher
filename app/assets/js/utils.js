a54f1ffb84a9843183bffd8f88ea87c0907181fd
andre <61792917+luuxis@users.noreply.github.com>
2021-06-30 01:37:25 +0200

    fix name error

--- app/assets/js/utils.js
+++ app/assets/js/utils.js
@@ -2,5 +2,5 @@ module.exports = {
     config: require("./utils/config.js"),
     auth: require("./utils/auth.js"),
     status_server: require("./utils/status_server/lib/index.js"),
-    microsoft: require("./utils/mincrosoft/microsoft.js")
+    microsoft: require("./utils/microsoft/microsoft.js")
 }
