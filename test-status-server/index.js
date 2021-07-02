0a15a09e9899a8f2ec92fc3616d926587fa64ba2
andre <61792917+luuxis@users.noreply.github.com>
2021-07-02 18:29:43 +0200

    update

--- test-status-server/index.js
+++ test-status-server/index.js
@@ -1,12 +1,13 @@
 const Gamedig = require('gamedig');
-const fs = require("fs")
-
 Gamedig.query({
     type: 'minecraft',
     host: 'jouer.arche-rp.fr'
 }).then((state) => {
-    let dataStringified = JSON.stringify(state);
-    fs.writeFileSync("./status.json", dataStringified);
+    status_json = state.raw.vanilla;
+    console.log(status_json.raw.players.online + " joueur(s) actuellement connectés");
+
+
+    document.getElementById("online").innerHTML = status_json.raw.players.online + " joueur(s) actuellement connect\u00e9(s)";
 }).catch((error) => {
     console.log("Server is offline");
 });
