1ec057142ec47277da99dcdd677ee52a624c5655
andre <61792917+luuxis@users.noreply.github.com>
2021-07-03 19:18:09 +0200

    fix

--- app/assets/js/panels/home.js
+++ app/assets/js/panels/home.js
@@ -20,10 +20,10 @@ config.info().then(config => {
         status_json = state.raw.vanilla;
         player = "luuxis"
         document.getElementById("online").innerHTML = status_json.raw.players.online + " joueur(s) actuellement connect\u00e9(s)";
-        console.log(status_json.raw.players.online + " joueur(s) actuellement connectés");
+        console.log(status_json.raw.players.online + " joueur(s) actuellement connect\u00e9(s)");
         for (let pas = 0; pas < status_json.raw.players.online; pas++) { 
           player = status_json.raw.players.sample[pas].name
-          //document.getElementById("users").innerHTML += `<img src="https://mc-heads.net/head/${player}" class="users"><b class="users"> ${player}</b></br>`
+          document.getElementById("users").innerHTML += `<img src="https://mc-heads.net/head/${player}" class="users"><b class="users"> ${player}</b></br>`
         }
        }).catch((error) => {
         document.getElementById("online").innerHTML = "Le serveur est ferme.";
