b373c832d55389e6a51eb7ff024082026b3adfd4
andre <61792917+luuxis@users.noreply.github.com>
2021-07-16 22:09:21 +0200

    fix error start

--- app/assets/js/utils.js
+++ app/assets/js/utils.js
@@ -1,6 +1,5 @@
 module.exports = {
     config: require("./utils/config.js"),
-    auth: require("./utils/auth.js"),
     status_server: require("./utils/status_server/lib/index.js"),
     microsoft: require("./utils/microsoft/microsoft.js")
 }
