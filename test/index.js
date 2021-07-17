8a520e2b49d565d7563b42a400cc4fccc916d2ef
andre <61792917+luuxis@users.noreply.github.com>
2021-07-17 15:15:51 +0200

    add new code

--- test/index.js
+++ test/index.js
@@ -5,36 +5,52 @@ java = require("../web/launcher/jre-download.json")
 minecraft = "1.17"
 
 
-if ((minecraft) >= "1.17"){
-  jdk = java.jre16
+function compare(v1, v2) {
+  if (v1===v2)
+    return 0;
+  const nbs1 = v1.split(".");
+  const nbs2 = v2.split(".");
+  const nbElem = Math.max(nbs1.length, nbs1.length)
+  for(i=0;i<nbElem ;++i) {
+      if(nbs2[i] === undefined)
+           return 1;
+      if(nbs1[i] === undefined)
+           return -1;
+      let nb1 = parseInt(nbs1[i]);
+      let nb2 = parseInt(nbs2[i]);
+      if(nb1 > nb2)
+           return 1;
+      if(nb1 < nb2)
+           return -1;
+  }
+  if(nbs2.length > nbs1.length)
+       return -1;
+  return 0;
+}
+
+
+if(compare(minecraft, "1.17") == 1){
   if(["win32"].includes(process.platform)){
-      url = jdk.windows
-      files = "OpenJDK16U-jre_x64_windows_hotspot_16.0.1_9.zip"
+      url = java.jre16.windows
     } else if(["darwin"].includes(process.platform)){
-      url = jdk.mac
-      files = "OpenJDK16U-jre_x64_mac_hotspot_16.0.1_9.tar.gz"
+      url = java.jre16.mac
     } else if(["linux"].includes(process.platform)){
-      url = jdk.linux  
-      files = "OpenJDK16U-jre_x64_linux_hotspot_16.0.1_9.tar.gz"   
+      url = java.jre16.linux    
     }
 } else {
-  jdk = java.jre8
   if(["win32"].includes(process.platform)){
-      url = jdk.windows
-      files = "OpenJDK8U-jre_x64_windows_hotspot_8u292b10.zip"
+      url = java.jre8.windows
     } else if(["darwin"].includes(process.platform)){
-      url = jdk.mac
-      files = "OpenJDK8U-jre_x64_mac_hotspot_8u292b10.tar.gz"
+      url = java.jre8.mac
     } else if(["linux"].includes(process.platform)){
-      url = jdk.linux 
-      files = "OpenJDK8U-jre_x64_linux_hotspot_8u292b10.tar.gz"   
+      url = java.jre8.linux   
     }
 }
 
 
 async function test(){
   await download(url, '.arche');
-  decompress('.arche/' + files, '.arche')
+  //decompress('.arche/' + files, '.arche')
 }
 
 
