30cc00eb2dff211789f5a152581bd930a9b852ca
andre <61792917+luuxis@users.noreply.github.com>
2021-07-17 04:03:19 +0200

    upddate

--- test/index.js
+++ test/index.js
@@ -1,29 +1,30 @@
-java = require("./java.json")
+java = require("../web/launcher/jre-download.json")
 minecraft = "1.17"
 
+
 if ((minecraft) >= "1.17"){
-    jdk = java.jre16
-    if(["win32"].includes(process.platform)){
-        url = jdk.windows
-      } else if(["darwin"].includes(process.platform)){
-        url = jdk.mac
-      } else if(["linux"].includes(process.platform)){
-        url = jdk.linux      
-      }
+  jdk = java.jre16
+  if(["win32"].includes(process.platform)){
+      url = jdk.windows
+    } else if(["darwin"].includes(process.platform)){
+      url = jdk.mac
+    } else if(["linux"].includes(process.platform)){
+      url = jdk.linux      
+    }
 } else {
-    jdk = java.jre8
-    if(["win32"].includes(process.platform)){
-        url = jdk.windows
-      } else if(["darwin"].includes(process.platform)){
-        url = jdk.mac
-      } else if(["linux"].includes(process.platform)){
-        url = jdk.linux      
-      }
+  jdk = java.jre8
+  if(["win32"].includes(process.platform)){
+      url = jdk.windows
+    } else if(["darwin"].includes(process.platform)){
+      url = jdk.mac
+    } else if(["linux"].includes(process.platform)){
+      url = jdk.linux      
+    }
 }
 
 const download = require('download');
  
 (async () => {
-    await download(url, 'dist');
+    await download(url, '.arche');
     
-})();
+})()
