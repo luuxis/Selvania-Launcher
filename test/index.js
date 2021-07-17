fee1e3956dfb5e6066cc209b72b275ac6a2696b8
andre <61792917+luuxis@users.noreply.github.com>
2021-07-17 03:37:01 +0200

    Update index.js

--- test/index.js
+++ test/index.js
@@ -21,18 +21,9 @@ if ((minecraft) >= "1.17"){
       }
 }
 
-const fs = require('fs');
 const download = require('download');
  
 (async () => {
-    await download('http://unicorn.com/foo.jpg', 'dist');
- 
-    fs.writeFileSync('dist/foo.jpg', await download('http://unicorn.com/foo.jpg'));
- 
-    download('unicorn.com/foo.jpg').pipe(fs.createWriteStream('dist/foo.jpg'));
- 
-    await Promise.all([
-        'unicorn.com/foo.jpg',
-        'cats.com/dancing.gif'
-    ].map(url => download(url, 'dist')));
+    await download(url, 'dist');
+    
 })();
