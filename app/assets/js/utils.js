4adfe28b8d69e8d8037572fdd26da91fd02daf3a
andre <61792917+luuxis@users.noreply.github.com>
2021-07-17 02:09:14 +0200

    fix error login

--- app/assets/js/utils.js
+++ app/assets/js/utils.js
@@ -1,5 +1,6 @@
 module.exports = {
     config: require("./utils/config.js"),
+    auth: require("./utils/auth.js"),
     status_server: require("./utils/status_server/lib/index.js"),
     microsoft: require("./utils/microsoft/microsoft.js")
 }
