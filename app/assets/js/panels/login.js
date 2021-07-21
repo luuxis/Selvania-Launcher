8087e87dd6759b699ee9f258f943e924a9427578
andre <61792917+luuxis@users.noreply.github.com>
2021-07-22 01:36:18 +0200

    Revert "goldfrite-v1 (#6)"
    This reverts commit f1f999397c93bff4707b388716dab0dab514127c.

--- app/assets/js/panels/login.js
+++ app/assets/js/panels/login.js
@@ -3,139 +3,133 @@ const fs = require("fs")
 const dataDirectory = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Application Support' : process.env.HOME)
 
 function login() {
-  if (document.querySelector(".pseudo").value == "" || document.querySelector(".password").value == "") {
-    document.querySelector(".error").style.display = "contents";
-    // J'ai ajouté ce code pour refaire une partie du design
-    document.querySelector(".modal").style.opacity = "1"
-    setTimeout(hideModal, 5000)
-    function hideModal() {
-      document.querySelector(".modal").style.opacity = "0"
+    if (document.querySelector(".pseudo").value == "" || document.querySelector(".password").value == ""){
+      document.querySelector(".error").style.display = "contents";
+      return;
     }
-    // ---
-    return;
-  }
-  document.querySelector(".pseudo").disabled = true;
-  document.querySelector(".password").disabled = true;
-  document.querySelector(".error").style.display = "none";
-  document.querySelector(".connexion").style.display = "contents";
-  auth.login(document.querySelector(".pseudo").value, document.querySelector(".password").value).then(user => {
-    config.config().then(config => {
-      const patch = (dataDirectory + "/" + config.dataDirectory)
-
-      let data = {
-        "user": {
-          "type": "mojang",
-          "access_token": user.access_token,
-          "client_token": user.client_token,
-          "uuid": user.uuid,
-          "name": user.name
-        }
-      };
-
-      let dataStringified = JSON.stringify(data, null, 2);
-
-      if (!fs.existsSync(patch)) {
-        fs.mkdirSync(patch);
-      }
-
-      fs.writeFileSync(patch + "/account.json", dataStringified);
-      window.location.href = "./home.html";
-    })
-  }).catch(err => {
-    document.querySelector(".connexion").style.display = "none";
-    document.querySelector(".pseudo").disabled = false;
-    document.querySelector(".password").disabled = false;
-    document.querySelector(".error").style.display = "contents";
-  })
-}
-
-function loginoff() {
-  if (document.querySelector(".pseudo").value == "") {
-    document.querySelector(".error").style.display = "contents";
-    return;
-  }
-  document.querySelector(".connexion").style.display = "contents";
-  document.querySelector(".pseudo").disabled = true;
-  document.querySelector(".password").disabled = true;
-  document.querySelector(".error").style.display = "none";
-  auth.login(document.querySelector(".pseudo").value, document.querySelector(".password").value).then(user => {
-    config.config().then(config => {
-      const patch = (dataDirectory + "/" + config.dataDirectory)
-
-      let data = {
-        "user": {
-          "type": "offline",
-          "pseudo": user.name
-        }
-      };
-
-      let dataStringified = JSON.stringify(data, null, 2);
-
-      if (!fs.existsSync(patch)) {
-        fs.mkdirSync(patch);
-      }
-
-      fs.writeFileSync(patch + "/account.json", dataStringified);
-      window.location.href = "./home.html";
-    })
-  }).catch(err => {
-    document.querySelector(".connexion").style.display = "none";
-    document.querySelector(".pseudo").disabled = false;
-    document.querySelector(".password").disabled = false;
-    document.querySelector(".error").style.display = "contents";
-  })
-}
-
-function connecte_on() {
-  window.location.href = "./login-online.html";
-}
-
-function microsoft_account() {
-  microsoft.getNWjs().FastLaunch(
-    (call) => {
+    document.querySelector(".pseudo").disabled = true;
+    document.querySelector(".password").disabled = true;
+    document.querySelector(".error").style.display = "none";
+    document.querySelector(".connexion").style.display = "contents";
+    auth.login(document.querySelector(".pseudo").value, document.querySelector(".password").value).then(user => {
       config.config().then(config => {
         const patch = (dataDirectory + "/" + config.dataDirectory)
-        const login = (patch + "/account.json")
-
-        let data = {
+    
+        let data = { 
           "user": {
-            "type": "xbox",
-            call
+            "type": "mojang",
+            "access_token": user.access_token,
+            "client_token": user.client_token,
+            "uuid": user.uuid,
+            "name": user.name
           }
-        };
-
+        }; 
+        
         let dataStringified = JSON.stringify(data, null, 2);
-
-        if (!fs.existsSync(patch)) {
+        
+        if(!fs.existsSync(patch)){
           fs.mkdirSync(patch);
         }
-
-        fs.writeFileSync(login, dataStringified);
-        console.log(patch);
+        
+        fs.writeFileSync(patch + "/account.json", dataStringified);
         window.location.href = "./home.html";
       })
-    },
-    (update) => {
-      switch (update.type) {
-        case "Starting":
-          document.querySelector(".connexion").style.display = "contents";
-          break;
-        case "Loading":
-          document.getElementById("microsoft_account_txt").innerHTML = "Connexion: " + update.percent + "%"
-          break;
-        case "Rejection":
-          document.getElementById("microsoft_account_txt").innerHTML = update.data
-          break;
-        case "Error":
-          document.getElementById("microsoft_account_txt").innerHTML = update.data
-          break;
-      }
-    }
-  )
-}
-
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Enter') {
-    login()
+    }).catch (err => {
+      document.querySelector(".connexion").style.display = "none";
+      document.querySelector(".pseudo").disabled = false;
+      document.querySelector(".password").disabled = false;
+      document.querySelector(".error").style.display = "contents";
+    })
   }
-})
+
+  function loginoff() {
+    if (document.querySelector(".pseudo").value == ""){
+      document.querySelector(".error").style.display = "contents";
+      return;
+    }
+    document.querySelector(".connexion").style.display = "contents";
+    document.querySelector(".pseudo").disabled = true;
+    document.querySelector(".password").disabled = true;
+    document.querySelector(".error").style.display = "none";
+    auth.login(document.querySelector(".pseudo").value, document.querySelector(".password").value).then(user => {
+      config.config().then(config => {
+        const patch = (dataDirectory + "/" + config.dataDirectory)
+    
+        let data = { 
+          "user": {
+            "type": "offline",
+            "pseudo": user.name
+          }
+        }; 
+        
+        let dataStringified = JSON.stringify(data, null, 2);
+        
+        if(!fs.existsSync(patch)){
+          fs.mkdirSync(patch);
+        }
+        
+        fs.writeFileSync(patch + "/account.json", dataStringified);
+        window.location.href = "./home.html";
+      })
+    }).catch (err => {
+      document.querySelector(".connexion").style.display = "none";
+      document.querySelector(".pseudo").disabled = false;
+      document.querySelector(".password").disabled = false;
+      document.querySelector(".error").style.display = "contents";
+    })
+  }
+
+  function connecte_on() {
+    window.location.href = "./login-online.html";
+  }
+
+  function microsoft_account(){
+    microsoft.getNWjs().FastLaunch(
+      (call) => {
+        config.config().then(config => {
+          const patch = (dataDirectory + "/" + config.dataDirectory)
+          const login = (patch + "/account.json" )
+      
+          let data = { 
+            "user": {
+              "type": "xbox",
+              call
+            }
+          };
+          
+          let dataStringified = JSON.stringify(data, null, 2);
+          
+          if(!fs.existsSync(patch)){
+            fs.mkdirSync(patch);
+          }
+          
+          fs.writeFileSync(login, dataStringified);
+          console.log(patch);
+          window.location.href = "./home.html";
+        })
+      },
+      (update) => {
+        switch (update.type) {
+          case "Starting":
+            document.querySelector(".connexion").style.display = "contents";
+            break;
+          case "Loading":
+            document.getElementById("microsoft_account_txt").innerHTML = "Connexion: " + update.percent + "%"
+            break;
+          case "Rejection":
+            document.getElementById("microsoft_account_txt").innerHTML = update.data
+            break;
+          case "Error":
+            document.getElementById("microsoft_account_txt").innerHTML = update.data
+            break;	
+        }
+      }
+    )
+  }
+
+  document.addEventListener('keydown', function (e) {
+    if(e.key === 'Enter'){
+      login()
+    }
+  })
+  
