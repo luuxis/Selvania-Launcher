9c1f5b627826718b23888ba6e45b9ed6768b7c64
andre <61792917+luuxis@users.noreply.github.com>
2021-09-11 16:28:42 +0200

    add class

--- app/assets/js/launcher.js
+++ app/assets/js/launcher.js
@@ -2,40 +2,41 @@ const fs = require("fs");
 let win = nw.Window.get();
 
 
+
 function createPanels(...panels){
-    let panelsElem = document.querySelector("#panels");
-    for(let panel of panels){
-      console.log(`Initializing ${panel} Panel...`);
-      let div = document.createElement("div");
-      div.classList.add("panel", panel);
-      div.innerHTML = fs.readFileSync(`app/panels/${panel}.html`, "utf8");
-      panelsElem.appendChild(div);
-    }
+  let panelsElem = document.querySelector("#panels");
+  for(let panel of panels){
+    console.log(`Initializing ${panel} Panel...`);
+    let div = document.createElement("div");
+    div.classList.add("panel", panel);
+    div.innerHTML = fs.readFileSync(`app/panels/${panel}.html`, "utf8");
+    panelsElem.appendChild(div);
   }
+}
 
-  function initFrame(){
-    document.querySelector(".frame").classList.toggle("hide");
-    document.querySelector(".dragbar").classList.toggle("hide");
+function initFrame(){
+  document.querySelector(".frame").classList.toggle("hide");
+  document.querySelector(".dragbar").classList.toggle("hide");
+  
+  document.querySelector("#minimize").addEventListener("click", () => {
+    win.minimize();
+  });
 
-    document.querySelector("#minimize").addEventListener("click", () => {
-      win.minimize();
-    });
+  let maximized = false;
+  let maximize = document.querySelector("#maximize");
+  maximize.addEventListener("click", () => {
+    if(maximized) win.unmaximize();
+    else win.maximize();
+    maximized = !maximized;
+    maximize.classList.toggle("icon-maximize");
+    maximize.classList.toggle("icon-restore-down");
+  });
 
-    let maximized = false;
-    let maximize = document.querySelector("#maximize");
-    maximize.addEventListener("click", () => {
-      if(maximized) win.unmaximize();
-      else win.maximize();
-      maximized = !maximized;
-      maximize.classList.toggle("icon-maximize");
-      maximize.classList.toggle("icon-restore-down");
-    });
+  document.querySelector("#close").addEventListener("click", () => {
+    win.close();
+  })
+}
 
-    document.querySelector("#close").addEventListener("click", () => {
-      win.close();
-    })
-  }
-
-  if(process.platform == "win32") initFrame()
-
-  createPanels('login', 'home', 'settings')
+if(process.platform == "win32") initFrame()
+createPanels('login', 'home', 'settings')
+//document.querySelector(".login").style.display = "block";
