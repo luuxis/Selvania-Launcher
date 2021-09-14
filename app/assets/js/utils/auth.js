aa47c7b3038dc9db9edef58c5297f9ee41477fd7
andre <61792917+luuxis@users.noreply.github.com>
2021-09-14 12:05:25 +0200

    fix error save cache login

--- app/assets/js/utils/auth.js
+++ app/assets/js/utils/auth.js
@@ -1,14 +1,9 @@
 const { MCAuth } = require('emc-core-luuxis');
 
-let authenticator;
-
-module.exports.user = authenticator;
-
 module.exports.login = function(username, password){
-
     return new Promise((resolve, reject) => {
         MCAuth.auth(username, password).then(user => {
-            authenticator = user;
+            module.exports.user = user
             return resolve(user);
         }).catch(error => {
             return reject (error);
