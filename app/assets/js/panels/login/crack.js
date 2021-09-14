31e6c8b834fa4ddc1dcd1a0893897802d7acaea5
andre <61792917+luuxis@users.noreply.github.com>
2021-09-14 18:26:06 +0200

    add settins login

--- app/assets/js/panels/login/crack.js
+++ app/assets/js/panels/login/crack.js
@@ -1,10 +1,29 @@
-const { config, auth, microsoft } = require('./assets/js/utils.js');
+const { auth } = require('./assets/js/utils.js');
 
 document.querySelector(`.password`).style.display = "none"
-
+document.querySelector(`.microsoft`).style.display = "none"
+document.getElementsByName('pseudo')[0].placeholder='nom d\'utilisateur';
 
 document.querySelector(".login-btn").addEventListener("click", () => {
-    auth.login(document.querySelector(".pseudo").value).then(user => {
+    if (document.querySelector(".pseudo").value == ""){
+        document.querySelector(".info-login").innerHTML = "Entrez votre adresse email / nom d'utilisateur"
+        document.querySelector(".info-login").style.color = "red";
+        document.querySelector(".info-login").style.display = "block"
+        return;
+    }
+
+    document.querySelector(".login-btn").disabled = true
+    document.querySelector(".pseudo").disabled = true
+    document.querySelector(".info-login").style.color = "#000000";
+    document.querySelector(".info-login").innerHTML = "Connexion en cours..."
+    document.querySelector(".info-login").style.display = "block"
+    auth.loginMojang(document.querySelector(".pseudo").value).then(user => {
         changePanel("login", "home")
+    }).catch (err => {
+        document.querySelector(".login-btn").disabled = false
+        document.querySelector(".pseudo").disabled = false
+        document.querySelector(".info-login").innerHTML = "error"
+        document.querySelector(".info-login").style.color = "red";
+        document.querySelector(".info-login").style.display = "block"
     })
 })
