2dd55d5f1d7a862626340cf27c265fcbaab9abfc
andre <61792917+luuxis@users.noreply.github.com>
2021-09-14 05:31:43 +0200

    add info crack and no crack login console

--- app/assets/js/launcher.js
+++ app/assets/js/launcher.js
@@ -1,30 +1,30 @@
 const fs = require("fs");
-const { config } = require('./assets/js/utils.js');
-let win = nw.Window.get();
+let win = nw.Window.get()
 
 
 if(process.platform == "win32") {
-  document.querySelector(".frame").classList.toggle("hide");
-  document.querySelector(".dragbar").classList.toggle("hide");
+  document.querySelector(".frame").classList.toggle("hide")
+  document.querySelector(".dragbar").classList.toggle("hide")
   
   document.querySelector("#minimize").addEventListener("click", () => {
-    win.minimize();
+    win.minimize()
   });
 
   let maximized = false;
-  let maximize = document.querySelector("#maximize");
+  let maximize = document.querySelector("#maximize")
   maximize.addEventListener("click", () => {
-    if(maximized) win.unmaximize();
-    else win.maximize();
-    maximized = !maximized;
-    maximize.classList.toggle("icon-maximize");
-    maximize.classList.toggle("icon-restore-down");
+    if(maximized) win.unmaximize()
+    else win.maximize()
+    maximized = !maximized
+    maximize.classList.toggle("icon-maximize")
+    maximize.classList.toggle("icon-restore-down")
   });
 
   document.querySelector("#close").addEventListener("click", () => {
     win.close();
   })
 }
+
 function changePanel(V1, V2){
   if(V1 == ""){
     document.querySelector(`.${V2}`).style.display = "block"
@@ -35,17 +35,15 @@ function changePanel(V1, V2){
 }
 
 (function(...panels){
-  let panelsElem = document.querySelector("#panels");
+  let panelsElem = document.querySelector("#panels")
   for(let panel of panels){
-    console.log(`Initializing ${panel} Panel...`);
-    let div = document.createElement("div");
-    div.classList.add("panel", panel);
-    div.innerHTML = fs.readFileSync(`app/panels/${panel}.html`, "utf8");
+    console.log(`Initializing ${panel} Panel...`)
+    let div = document.createElement("div")
+    div.classList.add("panel", panel)
+    div.innerHTML = fs.readFileSync(`app/panels/${panel}.html`, "utf8")
     panelsElem.appendChild(div);
     import (`./panels/${panel}.js`)
   }
-})('login', 'home', 'settings');
+})('login', 'home', 'settings')
 
 changePanel("", "login")
-  
-
--- app/assets/js/panels/login.js
+++ app/assets/js/panels/login.js
@@ -2,11 +2,11 @@ const { config } = require('./assets/js/utils.js');
 
 config.isonline().then(online => {
   if (online) {
-    console.log("Loading online login \(officiel login\)");
+    console.log(`Initializing Panel crack ${!online}`)
     import ("./login/microsoft.js")
     import ("./login/mojang.js")
   } else {
-    console.log("Loading offline login \(crack login\)");
+    console.log(`Initializing Panel crack ${!online}`)
     import ("./login/crack.js")
   }
 })
