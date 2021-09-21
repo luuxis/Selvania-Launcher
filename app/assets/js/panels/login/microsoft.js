7daad53c25e0ea86e37509274a0b673630f8e5e8
andre <61792917+luuxis@users.noreply.github.com>
2021-09-21 20:21:44 +0200

    add interface

--- app/assets/js/panels/login/microsoft.js
+++ app/assets/js/panels/login/microsoft.js
@@ -5,8 +5,8 @@ document.querySelector(".microsoft-btn").addEventListener("click", () => {
     document.querySelector(".login-btn").disabled = true
     document.querySelector(".pseudo").disabled = true
     document.querySelector(".password").disabled = true
-    document.querySelector(".info-login").style.color = "#000000";
-    document.querySelector(".info-login").innerHTML = "Connexion en cours..."
+    //document.querySelector(".info-login").style.color = "#000000";
+    //document.querySelector(".info-login").innerHTML = "Connexion en cours..."
     auth.loginMicrosoft().then(user => {
         config.config().then(res => {
             if(document.querySelector(".loginRemember").checked == true){
