be0b750b34230496d1e70618c965d69a672e1d83
andre <61792917+luuxis@users.noreply.github.com>
2021-09-22 14:00:52 +0200

    finish login

--- app/assets/js/panels/login/microsoft.js
+++ app/assets/js/panels/login/microsoft.js
@@ -5,8 +5,11 @@ document.querySelector(".microsoft-btn").addEventListener("click", () => {
     document.querySelector(".login-btn").disabled = true
     document.querySelector(".pseudo").disabled = true
     document.querySelector(".password").disabled = true
-    //document.querySelector(".info-login").style.color = "#000000";
-    //document.querySelector(".info-login").innerHTML = "Connexion en cours..."
+    document.querySelector(".uzurion-mail").innerHTML = "&nbsp;"
+    document.querySelector(".uzurion-password").innerHTML = "&nbsp;"
+    document.querySelector(".info-login").style.color = "white";
+    document.querySelector(".info-login").innerHTML = "Connexion en cours..."
+    document.querySelector(".info-login").style.display = "block"
     auth.loginMicrosoft().then(user => {
         config.config().then(res => {
             if(document.querySelector(".loginRemember").checked == true){
@@ -18,7 +21,7 @@ document.querySelector(".microsoft-btn").addEventListener("click", () => {
         })
         changePanel("login", "home")
     }).catch (err => {
-        console.log(err)
+        document.querySelector(".info-login").innerHTML = "&nbsp;"
         document.querySelector(".login-btn").disabled = false
         document.querySelector(".pseudo").disabled = false
         document.querySelector(".password").disabled = false
