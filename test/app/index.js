9d4a024e3ae57a942541815d2cfcd0b12e5dedf0
andre <61792917+luuxis@users.noreply.github.com>
2021-09-25 15:56:47 +0200

    finish news

--- test/app/index.js
+++ test/app/index.js
@@ -1,22 +1,32 @@
-const fetch = require("node-fetch")
-let newsForm = document.getElementById("news")
+const config = require("../../web/launcher/news-launcher/news-launcher.json")
+let newsForm = document.querySelector(".news")
 
 news()
 
 
 async function news () {
-let response = await fetch("http://minecraft-launcher.medianewsonline.com/servers/pentagone2/news/news.json").then(res => res.json());
-for (let i = 0; i < response.news.length; i++) {
-        var newsTitle = response.news[i].title
-        var newsBody = response.news[i].body
-        var newsImage = response.news[i].img
-        var newsAuthor = response.news[i].author
-        var newsDate = response.news[i].date
+for (let i = 0; i < config.news.length; i++) {
+        var newsTitle = config.news[i].title
+        var newsBody = config.news[i].body
+        var newsImage = config.news[i].img
+        var newsAuthor = config.news[i].author
+        var newsDate = config.news[i].date
         
         if (newsImage === null) {
-            newsForm.innerHTML += "<div class='news-title-form'><p class='news-title'><b>" + newsTitle + "</b></p><div class='news-body-form'><p style='line-height: 25px; margin: 0px;'>" + newsBody + "</p></div><p class='news-date'>Par " + newsAuthor + ", le " + newsDate + "</p></div>"
+            newsForm.innerHTML += `
+            <div class="newsTitle">${newsTitle}</div>
+            <div class="newsTitle">${newsBody}</div>
+            <div class="newsTitle">${newsAuthor}</div>
+            <div class="newsTitle">${newsDate}</div>
+            `
         } else {
-            newsForm.innerHTML += "<div class='news-title-form'><p class='news-title'><b>" + newsTitle + "</b></p><div class='news-body-form'><p style='line-height: 25px; margin: 0px;'>" + newsBody + "</p><img class='news-img' src='" + newsImage + "' alt='image'></div><p class='news-date'>Par " + newsAuthor + ", le " + newsDate + "</p></div>"
+            newsForm.innerHTML += `
+            <div class="newsTitle">${newsTitle}</div>
+            <div class="newsTitle">${newsBody}</div>
+            <img class="newsTitle" src="${newsImage}" alt="">
+            <div class="newsTitle">${newsAuthor}</div>
+            <div class="newsTitle">${newsDate}</div>
+            `
         }
     }
 }
