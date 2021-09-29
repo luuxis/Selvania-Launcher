badae2a02f0a96003c82b19ddf8bf29edfdb6227
andre <61792917+luuxis@users.noreply.github.com>
2021-09-29 19:25:22 +0200

    del settins btn

--- app/assets/js/panels/home.js
+++ app/assets/js/panels/home.js
@@ -1,7 +1,7 @@
-//import ("./home/news_server.js")
+import ("./home/news_server.js")
 import ("./home/start_game.js")
-//import ("./home/status_server.js")
+import ("./home/status_server.js")
 
-document.querySelector(".settings-btn").addEventListener("click", () => {
-    changePanel("home", "settings")
-})
+// document.querySelector(".settings-btn").addEventListener("click", () => {
+//     changePanel("home", "settings")
+// })
