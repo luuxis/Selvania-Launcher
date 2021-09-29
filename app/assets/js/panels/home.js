ac181efd8f95c8e3c48a29a8cff4ae2cc7d95720
andre <61792917+luuxis@users.noreply.github.com>
2021-09-29 21:58:18 +0200

    add settings btn

--- app/assets/js/panels/home.js
+++ app/assets/js/panels/home.js
@@ -2,6 +2,6 @@ import ("./home/news_server.js")
 import ("./home/start_game.js")
 import ("./home/status_server.js")
 
-// document.querySelector(".settings-btn").addEventListener("click", () => {
-//     changePanel("home", "settings")
-// })
+document.querySelector(".settings-btn").addEventListener("click", () => {
+    changePanel("home", "settings")
+})
