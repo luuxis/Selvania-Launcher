6ecfc3832c6012b97ae0c5e409f02cb821fe8ead
andre <61792917+luuxis@users.noreply.github.com>
2021-10-04 05:09:21 +0200

    update

--- app/assets/js/panels/login.js
+++ app/assets/js/panels/login.js
@@ -2,11 +2,12 @@ const { config } = require('./assets/js/utils.js');
 
 config.isonline().then(online => {
   if (online) {
-    console.log(`Initializing Panel crack ${!online}`)
+    console.log(`Initializing microsoft Panel...`)
     import ("./login/microsoft.js")
+    console.log(`Initializing mojang Panel...`)
     import ("./login/mojang.js")
   } else {
-    console.log(`Initializing Panel crack ${!online}`)
+    console.log(`Initializing crack Panel...`)
     import ("./login/crack.js")
   }
 })
