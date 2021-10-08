6ca671837a36eefab816215a2e483f9951a4020c
andre <61792917+luuxis@users.noreply.github.com>
2021-10-08 16:51:09 +0200

    .

--- app/assets/js/panels/settings/java-memory.js
+++ app/assets/js/panels/settings/java-memory.js
@@ -1,6 +1,5 @@
-const dataDirectory = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Application Support' : process.env.HOME)
 const os = require("os")
-const { config } = require('./assets/js/utils.js');
 
 const totalMem = Math.trunc(os.totalmem() / 1073741824 * 10) / 10;
 const freeMem = Math.trunc(os.freemem() / 1073741824 * 10) / 10;
+
