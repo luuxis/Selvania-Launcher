fa2d50a3ee98d063c095ca19e9f5f61f7831f4c7
andre <61792917+luuxis@users.noreply.github.com>
2021-10-15 21:21:45 +0200

    NewsAutoRefreshSettings

--- app/assets/js/panels/settings/settings-launcher.js
+++ app/assets/js/panels/settings/settings-launcher.js
@@ -1,36 +1,53 @@
 const { config } = require('./assets/js/utils.js');
 const dataDirectory = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Application Support' : process.env.HOME)
 
-
 config.config().then(res => {
     var file = require(`${dataDirectory}/${res.dataDirectory}/config.json`);
-    if (file.Launcher.StatusServerAutoRefresh === false) {
+    if (file.Launcher.NewsAutoRefresh === true) {
+        document.querySelector(".NewsAutoRefreshSettings").checked = true
+    } else if (file.Launcher.NewsAutoRefresh === false) {
+        document.querySelector(".NewsAutoRefreshSettings").checked = false
+    }
+
+    var file = require(`${dataDirectory}/${res.dataDirectory}/config.json`);
+    if (file.Launcher.StatusServerAutoRefresh === true) {
         document.querySelector(".StatusServerAutoRefreshSettings").checked = true
-    } else if (file.Launcher.StatusServerAutoRefresh === true) {
+    } else if (file.Launcher.StatusServerAutoRefresh === false) {
         document.querySelector(".StatusServerAutoRefreshSettings").checked = false
     }
 
-})
-
-config.config().then(res => {
     var file = require(`${dataDirectory}/${res.dataDirectory}/config.json`);
     if (file.Launcher.CloseLauncher === false) {
         document.querySelector(".CloseLauncherSettings").checked = true
     } else if (file.Launcher.CloseLauncher === true) {
         document.querySelector(".CloseLauncherSettings").checked = false
     }
-
 })
 
+
+
+document.querySelector(".NewsAutoRefreshSettings").addEventListener("click", () => {
+    config.config().then(res => {
+        var file = require(`${dataDirectory}/${res.dataDirectory}/config.json`);
+        if(document.querySelector(".NewsAutoRefreshSettings").checked == true){
+            file.Launcher.NewsAutoRefresh = true
+            fs.writeFileSync(`${dataDirectory}/${res.dataDirectory}/config.json`, JSON.stringify(file, true, 4), 'UTF-8')
+        } else if(document.querySelector(".NewsAutoRefreshSettings").checked ==  false){
+            file.Launcher.NewsAutoRefresh = false
+            fs.writeFileSync(`${dataDirectory}/${res.dataDirectory}/config.json`, JSON.stringify(file, true, 4), 'UTF-8')
+
+        }
+    })
+})
 
 document.querySelector(".StatusServerAutoRefreshSettings").addEventListener("click", () => {
     config.config().then(res => {
         var file = require(`${dataDirectory}/${res.dataDirectory}/config.json`);
         if(document.querySelector(".StatusServerAutoRefreshSettings").checked == true){
-            file.Launcher.StatusServerAutoRefresh = false
+            file.Launcher.StatusServerAutoRefresh = true
             fs.writeFileSync(`${dataDirectory}/${res.dataDirectory}/config.json`, JSON.stringify(file, true, 4), 'UTF-8')
         } else if(document.querySelector(".StatusServerAutoRefreshSettings").checked ==  false){
-            file.Launcher.StatusServerAutoRefresh = true
+            file.Launcher.StatusServerAutoRefresh = false
             fs.writeFileSync(`${dataDirectory}/${res.dataDirectory}/config.json`, JSON.stringify(file, true, 4), 'UTF-8')
 
         }
