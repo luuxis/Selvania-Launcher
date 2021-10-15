efc8294a871bdc32dfbc1a63bc61c194790437a4
andre <61792917+luuxis@users.noreply.github.com>
2021-10-15 21:14:08 +0200

    StatusServerAutoRefresh

--- app/assets/js/panels/settings/settings-launcher.js
+++ app/assets/js/panels/settings/settings-launcher.js
@@ -2,9 +2,15 @@ const { config } = require('./assets/js/utils.js');
 const dataDirectory = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Application Support' : process.env.HOME)
 
 
+config.config().then(res => {
+    var file = require(`${dataDirectory}/${res.dataDirectory}/config.json`);
+    if (file.Launcher.StatusServerAutoRefresh === false) {
+        document.querySelector(".StatusServerAutoRefreshSettings").checked = true
+    } else if (file.Launcher.StatusServerAutoRefresh === true) {
+        document.querySelector(".StatusServerAutoRefreshSettings").checked = false
+    }
 
-
-
+})
 
 config.config().then(res => {
     var file = require(`${dataDirectory}/${res.dataDirectory}/config.json`);
@@ -14,6 +20,21 @@ config.config().then(res => {
         document.querySelector(".CloseLauncherSettings").checked = false
     }
 
+})
+
+
+document.querySelector(".StatusServerAutoRefreshSettings").addEventListener("click", () => {
+    config.config().then(res => {
+        var file = require(`${dataDirectory}/${res.dataDirectory}/config.json`);
+        if(document.querySelector(".StatusServerAutoRefreshSettings").checked == true){
+            file.Launcher.StatusServerAutoRefresh = false
+            fs.writeFileSync(`${dataDirectory}/${res.dataDirectory}/config.json`, JSON.stringify(file, true, 4), 'UTF-8')
+        } else if(document.querySelector(".StatusServerAutoRefreshSettings").checked ==  false){
+            file.Launcher.StatusServerAutoRefresh = true
+            fs.writeFileSync(`${dataDirectory}/${res.dataDirectory}/config.json`, JSON.stringify(file, true, 4), 'UTF-8')
+
+        }
+    })
 })
 
 document.querySelector(".CloseLauncherSettings").addEventListener("click", () => {
