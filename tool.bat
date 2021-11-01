0033050a090657f51cd953f26997119d5b5b2741
andre <61792917+luuxis@users.noreply.github.com>
2021-11-01 16:25:56 +0100

    add arch 32 bits nodejs

--- tool.bat
+++ tool.bat
@@ -1,23 +1,23 @@
 @echo off
 set ver=16.13.0
+set arch=64
 set appdata=%CD%\AppData
-set url=https://nodejs.org/dist/v%ver%/node-v%ver%-win-x64.zip
 
-if not exist "%CD%\node-v%ver%-win-x64" mkdir "%CD%\node-v%ver%-win-x64"
-PATH=%PATH%;"%CD%\node-v%ver%-win-x64"
+if not exist "%CD%\node-v%ver%-win-x%arch%" mkdir "%CD%\node-v%ver%-win-x%arch%"
+PATH=%PATH%;"%CD%\node-v%ver%-win-x%arch%"
 title node-js portable version %ver% LTS
 
-if not exist "%CD%\node-v%ver%-win-x64\node.exe" (
+if not exist "%CD%\node-v%ver%-win-x%arch%\node.exe" (
     echo installation de node-js portable version %ver% LTS
     if not exist "%CD%\cache" mkdir "%CD%\cache"
 
-    powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://luuxis.legtux.org/res/craftdium/7z.exe' '%CD%\node-v%ver%-win-x64\7z.exe'"
-    powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://luuxis.legtux.org/res/craftdium/7z.dll' '%CD%\node-v%ver%-win-x64\7z.dll'"
-    powershell "Import-Module BitsTransfer; Start-BitsTransfer '%url%' '%CD%\cache\node-js.zip'"
+    powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://luuxis.legtux.org/res/craftdium/7z.exe' '%CD%\node-v%ver%-win-x%arch%\7z.exe'"
+    powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://luuxis.legtux.org/res/craftdium/7z.dll' '%CD%\node-v%ver%-win-x%arch%\7z.dll'"
+    powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://nodejs.org/dist/v%ver%/node-v%ver%-win-x%arch%.zip' '%CD%\cache\node-js.zip'"
 
     7z x -o"%CD%\" "%CD%\cache\node-js.zip"
-    del "%CD%\node-v%ver%-win-x64\7z.exe"
-    del "%CD%\node-v%ver%-win-x64\7z.dll"
+    del "%CD%\node-v%ver%-win-x%arch%\7z.exe"
+    del "%CD%\node-v%ver%-win-x%arch%\7z.dll"
     rmdir "%CD%\cache" /S /Q
 )
 :launch
