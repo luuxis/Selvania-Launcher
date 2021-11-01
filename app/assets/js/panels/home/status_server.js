ec963a63a772f890ee0e66e8136909d698ee284c
andre <61792917+luuxis@users.noreply.github.com>
2021-11-01 16:26:04 +0100

    Update status_server.js

--- app/assets/js/panels/home/status_server.js
+++ app/assets/js/panels/home/status_server.js
@@ -10,13 +10,13 @@ config.info().then(config => {
         var status_json = state.raw.vanilla
         document.querySelector(".player-connect").innerHTML = ""
         if(status_json.raw.players.online === 0){
-            document.querySelector(".player-connect-number").innerHTML = `Aucun joueur actuellement connect\u00e9`;
-            document.querySelector(".player-connect").innerHTML = `Aucun joueur actuellement connect\u00e9`;
+            document.querySelector(".player-connect-number").innerHTML = `Aucun joueur actuellement connecté`;
+            document.querySelector(".player-connect").innerHTML = `Aucun joueur actuellement connecté`;
         } else if (status_json.raw.players.online === 1){
-            document.querySelector(".player-connect-number").innerHTML = `${status_json.raw.players.online} joueur actuellement connect\u00e9`;
+            document.querySelector(".player-connect-number").innerHTML = `${status_json.raw.players.online} joueur actuellement connecté`;
             head(status_json)
         } else {
-            document.querySelector(".player-connect-number").innerHTML = `${status_json.raw.players.online} joueurs actuellement connect\u00e9s`;
+            document.querySelector(".player-connect-number").innerHTML = `${status_json.raw.players.online} joueurs actuellement connectés`;
             head(status_json)
         }
        }).catch((err) => {
