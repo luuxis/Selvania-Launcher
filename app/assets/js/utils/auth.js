ee4a6cf10090665a54ab3ae18ff1e45de049276e
Luuxistuto <luuxis8215@gmail.com>
2022-01-18 06:23:57 +0100

    fix

--- app/assets/js/utils/auth.js
+++ app/assets/js/utils/auth.js
@@ -3,7 +3,18 @@ const { mojang, microsoft } = require('minecraft-java-core');
 module.exports.loginMojang = function(username, password){
     return new Promise((resolve, reject) => {
         mojang.getAuth(username, password).then(user => {
-            module.exports.user = user
+            return resolve(user);
+        }).catch(error => {
+            return reject (error);
+        })
+    })
+}
+
+module.exports.refreshAuth = function(acc){
+    let access_token = acc.access_token;
+    let client_token = acc.client_token;
+    return new Promise((resolve, reject) => {
+        mojang.refreshAuth(access_token, client_token).then(user => {
             return resolve(user);
         }).catch(error => {
             return reject (error);
@@ -20,7 +31,7 @@ module.exports.getUser = function(id){
     for(let [uuid, value] of Users){
         let users = {}
         users = value
-        users.id = uuid
+        users.uuid = uuid
         user.push(users);
     }
     return user
@@ -30,7 +41,6 @@ module.exports.loginMicrosoft = function(id){
     const Microsoft = new microsoft(id);
     return new Promise((resolve, reject) => {
         Microsoft.getAuth().then(user => {
-            module.exports.user = user
             return resolve(user);
         }).catch(error => {
             return reject (error);
