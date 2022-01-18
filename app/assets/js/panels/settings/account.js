71f058fa92f8a65542933d20571354e4abc05d1d
Luuxistuto <luuxis8215@gmail.com>
2022-01-18 01:20:14 +0100

    update lib

--- app/assets/js/panels/settings/account.js
+++ app/assets/js/panels/settings/account.js
@@ -1,8 +1,7 @@
 document.querySelector(".singout").addEventListener("click", () => {
     config.config().then(res => {
         const file = require(`${dataDirectory}/${res.dataDirectory}/config.json`);
-        file.Login.UserConnect = null
-        file.Login.Account = null 
+        file.Login = null
         fs.writeFileSync(`${dataDirectory}/${res.dataDirectory}/config.json`, JSON.stringify(file, true, 4), 'UTF-8')
         changePanel("settings", "login")
     })
