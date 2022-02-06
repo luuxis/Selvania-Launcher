cbb6d1df82b76d877bb5a1175e23648dde316a01
Luuxis-tuto <61792917+luuxis@users.noreply.github.com>
2022-02-06 15:09:12 +0100

    fix news

--- app/assets/js/panels/home/news_server.js
+++ app/assets/js/panels/home/news_server.js
@@ -4,15 +4,15 @@ let newsForm = document.querySelector(".container-news")
 
 config.news().then(config => {
   newsForm.innerHTML = ``
-  if(config.news.length === 0){
+  if(config.length === 0){
     newsForm.innerHTML = `<div class="newsTitle">Aucune news n'est actuellement disponible.</div>`
   } else {
-    for (let i = 0; i < config.news.length; i++) {
-      var newsTitle = config.news[i].title
-      var newsBody = config.news[i].body
-      var newsImage = config.news[i].img
-      var newsAuthor = config.news[i].author
-      var newsDate = config.news[i].date
+    for (let i = 0; i < config.length; i++) {
+      var newsTitle = config[i].title
+      var newsBody = config[i].body
+      var newsImage = config[i].img
+      var newsAuthor = config[i].author
+      var newsDate = config[i].date
       
       if (newsImage === "") {
         newsForm.innerHTML += 
