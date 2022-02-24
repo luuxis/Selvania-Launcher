e79b74eece99bff2eec148b1512f2633046092c9
andre <61792917+luuxis@users.noreply.github.com>
2022-02-25 00:58:45 +0100

    update

--- tool.bat
+++ tool.bat
@@ -31,9 +31,9 @@ echo "all":    build launcher all platform
 set /P c=enter your choice: 
 if /I "%c%" EQU "node" cls && cmd
 if /I "%c%" EQU "start" cls && npm install && npm start
-if /I "%c%" EQU "win" cls && npm run build:win
-if /I "%c%" EQU "mac" cls && npm run build:mac
-if /I "%c%" EQU "linux" cls && npm run build:linux
-if /I "%c%" EQU "all" cls && npm run build:all
+if /I "%c%" EQU "win" cls && npm install && npm run build:win
+if /I "%c%" EQU "mac" cls && npm install && npm run build:mac
+if /I "%c%" EQU "linux" cls && npm install && npm run build:linux
+if /I "%c%" EQU "all" cls && npm install && npm run build:all
 goto :launch
 
