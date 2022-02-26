8b325f82fe3ee54e73c99ad7f5c27e70c29a706c
andre <61792917+luuxis@users.noreply.github.com>
2022-02-26 09:41:40 +0100

    finish global design

--- src/assets/js/panels/skin.js
+++ src/assets/js/panels/skin.js
@@ -1,6 +1,3 @@
-const { skin } = require('minecraft-java-core');
-const fs = require('fs');
-document.querySelector('.skin-file-button').addEventListener('click', () => {
-    let img = fs.readFileSync(document.querySelector('.skin-file').value)
-    console.log(img)
-})
+document.querySelector('.btn-cancel').addEventListener('click', () => {
+    changePanel("skin", "home")
+});
