7ef6cf5441ef3b9101b193f50bdc2e36bf01ad33
andre <61792917+luuxis@users.noreply.github.com>
2022-02-26 09:42:23 +0100

    add btn skin

--- src/assets/js/panels/home.js
+++ src/assets/js/panels/home.js
@@ -2,9 +2,9 @@ import ("./home/start_game.js")
 import ("./home/start_game.js")
 import ("./home/news_server.js")
 
-// document.querySelector(".user-head").addEventListener("click", () => {
-//     changePanel("home", "skin")
-// })
+document.querySelector(".user-head").addEventListener("click", () => {
+    changePanel("home", "skin")
+})
 
 document.querySelector(".settings-btn").addEventListener("click", () => {
     changePanel("home", "settings")
