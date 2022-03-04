60bd33b3bc1b2735670167ce4f2927e871c8a26f
andre <61792917+luuxis@users.noreply.github.com>
2022-03-04 18:00:28 +0100

    Update skins.js

--- src/assets/js/panels/skins.js
+++ src/assets/js/panels/skins.js
@@ -1,7 +1,4 @@
-document.querySelector(".user-head").addEventListener("click", () => {
-    changePanel("home", "skins")
-    import ("./skins/skin3D.js")
-})
+
 
 document.querySelector('.btn-cancel').addEventListener('click', () => {
     changePanel("skins", "home")
