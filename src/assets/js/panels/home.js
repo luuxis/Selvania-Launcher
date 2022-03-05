eade84441cbde5e440d3a35af9be238de8b4a245
andre <61792917+luuxis@users.noreply.github.com>
2022-03-05 01:21:58 +0100

    start V3

--- src/assets/js/panels/home.js
+++
@@ -1,8 +0,0 @@
-import ("./home/start_game.js")
-import ("./home/start_game.js")
-import ("./home/news_server.js")
-
-
-document.querySelector(".settings-btn").addEventListener("click", () => {
-    changePanel("home", "settings")
-})
