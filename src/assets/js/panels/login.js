4bd4e9377c60b3442b226c94c422e7677e772880
andre <61792917+luuxis@users.noreply.github.com>
2022-03-12 11:03:54 +0100

    fix capes save

--- src/assets/js/panels/login.js
+++ src/assets/js/panels/login.js
@@ -8,29 +8,18 @@ class Login {
     static id = "login";
     async init() {
         this.database = await new database().init();
+        this.config = await config.config().then(res => res);
 
-        this.link();
-        config.config().then(res => {
-            if (!res.online) this.online()
-            else this.offline()
-        })
+
+        if (this.config.online) this.online()
+        else this.offline()
+
+
         document.querySelector(".mojang").addEventListener("click", () => {
             document.querySelector(".login-card").style.display = "none";
             document.querySelector(".login-card-mojang").style.display = "block";
         })
-    }
 
-    online() {
-        this.loginmojang();
-        this.loginmicrosoft();
-    }
-
-    offline() {
-        this.loginoffline();
-        this.loginmicrosoft();
-    }
-
-    link() {
         document.querySelector(".store").addEventListener("click", () => {
             nw.Shell.openExternal("https://www.minecraft.net/store/minecraft-java-edition")
         })
@@ -39,10 +28,27 @@ class Login {
         })
     }
 
+    online() {
+        console.log(`Initializing microsoft Panel...`)
+        console.log(`Initializing mojang Panel...`)
+        this.loginmojang();
+        this.loginmicrosoft();
+    }
+
+    offline() {
+        console.log(`Initializing microsoft Panel...`)
+        console.log(`Initializing mojang Panel...`)
+        console.log(`Initializing offline Panel...`)
+        this.loginoffline();
+        this.loginmicrosoft();
+    }
+
     loginmojang() {}
     loginmicrosoft() {
         document.querySelector(".microsoft").addEventListener("click", () => {
             new microsoft().getAuth().then(user => {
+                if (user === null || user === undefined || !user) return;
+
                 this.database.add({
                     access_token: user.access_token,
                     client_token: user.client_token,
@@ -59,7 +65,7 @@ class Login {
                 this.database.add({
                     uuid: user.uuid,
                     skins: user.profile.skins,
-                    cape: user.profile.capes
+                    cape: user.profile.cape
                 }, 'profile')
             })
         })
