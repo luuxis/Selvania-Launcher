3bc69670d08bac9b876f3f8791f05c336e26d8de
andre <61792917+luuxis@users.noreply.github.com>
2022-03-13 14:47:07 +0100

    fix log

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -1,7 +1,9 @@
 import config from './utils/config.js';
 import database from './utils/database.js';
+import logger from './utils/logger.js';
 
 export {
     config as config,
-    database as database
+    database as database,
+    logger as logger
 }
