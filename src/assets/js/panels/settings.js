569c1db1d385a84fd3587b9104681f693b6ec870
andre <61792917+luuxis@users.noreply.github.com>
2022-03-26 14:41:09 +0100

    add btn del account

--- src/assets/js/panels/settings.js
+++ src/assets/js/panels/settings.js
@@ -1,12 +1,25 @@
 'use strict';
 
+import { database, changePanel } from '../utils.js';
+
 class Settings {
     static id = "settings";
-    init() {
+    async init() {
+        this.database = await new database().init();
+        this.initBtn();
+    }
+
+    initBtn() {
         document.querySelector('.accounts').addEventListener('click', (e) => {
             let uuid = e.target.id;
-            console.log(uuid);
-        });
+            if (e.target.classList.contains("account-delete")) {
+                this.database.delete(e.path[1].id, 'accounts');
+                document.querySelector('.accounts').removeChild(e.path[1])
+                if (document.querySelector('.accounts').children.length == 1) {
+                    changePanel("login");
+                }
+            }
+        })
     }
 }
 export default Settings;
