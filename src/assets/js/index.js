4e43e9b3d20de26489b0f12ad9251bc17e077d18
andre <61792917+luuxis@users.noreply.github.com>
2022-03-28 15:19:48 +0200

    fix logo

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,7 +22,8 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je.... vie...", "author": "Luuxis" }
+            { "message": "Je... vie...", "author": "Luuxis" },
+            { "message": "Salut je suis du code.", "author": "Luuxis" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
