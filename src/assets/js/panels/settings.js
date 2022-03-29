d59ff864425f526db7e7760f4616f29e6a8825c9
andre <61792917+luuxis@users.noreply.github.com>
2022-03-29 17:07:34 +0200

    add tabs settings

--- src/assets/js/panels/settings.js
+++ src/assets/js/panels/settings.js
@@ -6,6 +6,7 @@ class Settings {
     static id = "settings";
     async init() {
         this.database = await new database().init();
+        this.inittab();
         this.initBtn();
     }
 
@@ -15,7 +16,7 @@ class Settings {
             if (e.target.classList.contains("account-delete")) {
                 this.database.delete(e.path[1].id, 'accounts');
                 document.querySelector('.accounts').removeChild(e.path[1])
-                if (document.querySelector('.accounts').children.length == 1) {
+                if (!document.querySelector('.accounts').children.length) {
                     changePanel("login");
                 }
             }
@@ -25,6 +26,23 @@ class Settings {
             document.querySelector(".cancel-login").style.display = "contents";
             changePanel("login");
         })
+    }
+
+    inittab() {
+        let TabBtn = document.querySelectorAll('.tab-btn');
+        let TabContent = document.querySelectorAll('.tabs-settings-content');
+
+        for (let i = 0; i < TabBtn.length; i++) {
+            TabBtn[i].addEventListener('click', () => {
+                if (TabBtn[i].classList.contains('save-tabs-btn')) return
+                for (let j = 0; j < TabBtn.length; j++) {
+                    TabContent[j].classList.remove('active-tab-content');
+                    TabBtn[j].classList.remove('active-tab-btn');
+                }
+                TabContent[i].classList.add('active-tab-content');
+                TabBtn[i].classList.add('active-tab-btn');
+            });
+        }
     }
 }
 export default Settings;
