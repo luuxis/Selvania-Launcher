3ece7fbca7725f57f3055399ae42d9c625cb3dad
andre <61792917+luuxis@users.noreply.github.com>
2022-04-11 14:34:12 +0200

    add new

--- src/assets/js/utils/config.js
+++ src/assets/js/utils/config.js
@@ -1,11 +1,13 @@
 const pkg = nw.global.manifest.__nwjs_manifest;
 const fetch = require("node-fetch")
-
+const convert = require('xml-js');
 let url = pkg.user ? `${pkg.url}/${pkg.user}` : pkg.url
-const config = url + "/launcher/config-launcher/config.json";
+
+let config = `${url}/launcher/config-launcher/config.json`;
+let news = `${url}/launcher/news-launcher`;
 
 class Config {
-    config() {
+    GetConfig() {
         return new Promise((resolve, reject) => {
             fetch(config).then(config => {
                 return resolve(config.json());
@@ -13,6 +15,31 @@ class Config {
                 return reject(error);
             })
         })
+    }
+
+    async GetNews() {
+        let rss = await fetch(news).then(res => res.text());
+        let rssparse = JSON.parse(convert.xml2json(rss, { compact: true }));
+        let data = [];
+        if (rssparse.rss.channel.item.length) {
+            for (let i of rssparse.rss.channel.item) {
+                let item = {}
+                item.title = i.title._text;
+                item.content = i.content._text;
+                item.author = i.author._text;
+                item.publish_date = i.publish_date._text;
+                data.push(item);
+            }
+        } else {
+            let item = {}
+            item.title = rssparse.rss.channel.item.title._text;
+            item.content = rssparse.rss.channel.item.content._text;
+            item.author = rssparse.rss.channel.item.author._text;
+            item.publish_date = rssparse.rss.channel.item.publish_date._text;
+            data.push(item);
+
+        }
+        return data;
     }
 }
 
