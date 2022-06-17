100d3ee4cbe20819f82956dab7094ddfb10ab888
andre <61792917+luuxis@users.noreply.github.com>
2022-06-17 02:26:54 +0200

    update

--- launcher/news-launcher/assets/php/news/GetNews.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "news.php";
-$news = new News();
-$resultat  = $news->getAllNews();
-//print_r ($resultat);
-header('Content-Type: application/json');
-echo json_encode($resultat);
--- launcher/news-launcher/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(file_exists("config.ini")){
-    require("assets/php/panel.php");
-} else {
-    require("assets/php/install.php");
-}
-?>
-
-
