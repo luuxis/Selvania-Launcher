9b1ee6fbcfd6668d9398f3b4090b5c20700e5b26
andre <61792917+luuxis@users.noreply.github.com>
2022-06-20 00:35:04 +0200

    1.0.0 electron

--- build.js
+++ build.js
@@ -1,20 +1,6 @@
 const builder = require('electron-builder')
-const { url, preductname } = require('./package.json')
-const Platform = builder.Platform
+const { preductname } = require('./package.json')
 
-function getCurrentPlatform(){
-    switch(process.platform){
-        case 'win32':
-            return Platform.WINDOWS
-        case 'darwin':
-            return Platform.MAC
-        case 'linux':
-            return Platform.linux
-        default:
-            console.error('Cannot resolve current platform!')
-            return undefined
-    }
-}
 
 let configBuild = {
     appId: preductname,
@@ -29,9 +15,6 @@ let configBuild = {
         target: [{
             target: "nsis",
             arch: ["x64"]
-        }, {
-            target: "zip",
-            arch: ["x64"]
         }],
     },
     nsis: {
@@ -44,9 +27,6 @@ let configBuild = {
         target: [{
             target: "dmg",
             arch: ["x64", "arm64"]
-        }, {
-            target: "zip",
-            arch: ["x64", "arm64"]
         }]
     },
     linux: {
@@ -55,25 +35,13 @@ let configBuild = {
             target: "AppImage",
             arch: ["x64"]
         }, {
-            target: "deb",
-            arch: ["x64"]
-        }, {
-            target: "rpm",
-            arch: ["x64"]
-        }, {
-            target: "zip",
+            target: "tar.gz",
             arch: ["x64"]
         }]
-    },
-    // publish: {
-    //     provider: "generic",
-    //     url: `${url}/launcher/update-launcher`,
-    //     updaterCacheDirName: "${productName}-updaterer"
-    // }
+    }
 }
 
 builder.build({
-    // targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: configBuild
 }).then(() => {
     console.log('le build est terminé')
