755c4ad266109b11cca195ad76e207f2492fc1ea
Luuxis-tuto <61792917+luuxis@users.noreply.github.com>
2022-06-27 10:30:18 +0200

    add data temp folder dev

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const { app, ipcMain } = require('electron');
 const { Microsoft } = require('minecraft-java-core');
 const { autoUpdater } = require('electron-updater')
+
+const path = require('path');
+const fs = require('fs');
+
 const UpdateWindow = require("./assets/js/windows/updateWindow.js");
 const MainWindow = require("./assets/js/windows/mainWindow.js");
 
+let dev = process.env.NODE_ENV === 'dev';
+
+if (dev) {
+    let appPath = path.resolve('./AppData/Launcher').replace(/\\/g, '/');
+    if(!fs.existsSync(appPath)) fs.mkdirSync(appPath, { recursive: true });
+    app.setPath('userData', appPath);
+}
 
 const gotTheLock = app.requestSingleInstanceLock();
 
