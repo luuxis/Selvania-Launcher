a0cf353ba34c045747eb384fd5b02449aea1a15d
Luuxis-tuto <61792917+luuxis@users.noreply.github.com>
2022-07-01 12:19:25 +0200

    add for all files

--- src/assets/js/windows/mainWindow.js
+++ src/assets/js/windows/mainWindow.js
@@ -1,3 +1,8 @@
+/**
+ * @author Luuxis
+ * @license CC-BY-NC 4.0 - https://creativecommons.org/licenses/by-nc/4.0/
+ */
+
 "use strict";
 const electron = require("electron");
 const path = require("path");
