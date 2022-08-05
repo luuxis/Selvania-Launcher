13d72b8456b9b65178121f92759fc680b3a192c5
andre <61792917+luuxis@users.noreply.github.com>
2022-08-05 04:26:45 +0200

    add btn login

--- src/assets/js/panels/login.js
+++ src/assets/js/panels/login.js
@@ -9,6 +9,16 @@ class Login {
     static id = "login";
     async init(config) {
         this.config = config;
+        this.config.online ? this.getOnline() : this.getOffline()
+    }
+
+    getOnline() {
+        document.querySelector('.login-text p').innerHTML = 'Un compte Minecraft est requis pour jouer.';
+    }
+
+    getOffline() {
+        document.querySelector('.login-text p').innerHTML = 'Un compte Minecraft ou un pseudo est requis pour jouer.';
+        document.querySelector('.offline').style.display = 'block';
     }
 }
 export default Login;
