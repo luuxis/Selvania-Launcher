27ab446d2fadcc8f09ce19463287482cd23113d5
andre <61792917+luuxis@users.noreply.github.com>
2022-08-07 03:46:15 +0200

    fix bat file

--- tool.bat
+++ tool.bat
@@ -27,10 +27,6 @@ echo start:  start launcher
 set /P c=enter your choice: 
 if /I "%c%" EQU "node"  cls && cmd
 if /I "%c%" EQU "start" cls && npm install && npm start
-if /I "%c%" EQU "win"   cls && npm install && npm run build:win
-if /I "%c%" EQU "mac"   cls && npm install && npm run build:mac
-if /I "%c%" EQU "linux" cls && npm install && npm run build:linux
-if /I "%c%" EQU "all"   cls && npm install && npm run build:all
 cls
 echo une erreur est survenue
 goto :launch
