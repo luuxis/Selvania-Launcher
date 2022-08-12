b8d75fb90890dbe73c4f101c073c19245d773372
andre <61792917+luuxis@users.noreply.github.com>
2022-08-12 11:21:02 +0200

    fetch > nodeFetch

--- src/assets/js/utils/config.js
+++ src/assets/js/utils/config.js
@@ -4,7 +4,7 @@
  */
 
 const pkg = require('../package.json');
-const fetch = require("node-fetch")
+const nodeFetch = require("node-fetch")
 let url = pkg.user ? `${pkg.url}/${pkg.user}` : pkg.url
 
 let config = `${url}/launcher/config-launcher/config.json`;
@@ -12,7 +12,7 @@ let config = `${url}/launcher/config-launcher/config.json`;
 class Config {
     GetConfig() {
         return new Promise((resolve, reject) => {
-            fetch(config).then(config => {
+            nodeFetch(config).then(config => {
                 return resolve(config.json());
             }).catch(error => {
                 return reject(error);
