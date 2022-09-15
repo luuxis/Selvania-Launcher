c5a1bcc822d47245d877d67cb84ba8eca32003b1
SHADOWYT <41743141+SHADOWYT3@users.noreply.github.com>
2022-09-15 20:14:38 +0200

    0.0.1

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,9 +22,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Écoutez, est-ce la victoire que j'entends au loin ?", "author": "Jessie" },
+            { "message": "Bonjour les ennuis, nous sommes de retour.", "author": "Jessie" },
+            { "message": " Miaouss, oui la guerre !", "author": "Miaouss" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
