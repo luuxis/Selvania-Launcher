10aa73d678e9cee71dc3d15df6bd38811a579c6a
ChatNoir_040147 <50374203+ChatNoir-040147@users.noreply.github.com>
2022-10-22 22:24:05 +0200

    p
    p

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,9 +22,7 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Miraculous Ladybug !", "author": "Ladybug" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
