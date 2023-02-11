ebac3c71a1bd3d7ee639e2c82899823a5a4420a4
andre <61792917+luuxis@users.noreply.github.com>
2023-02-12 00:37:21 +0100

    Update index.php

--- files/index.php
+++ files/index.php
@@ -11,22 +11,8 @@ function dirToArray($dir) {
             $size = filesize($dir . "/" . $value);
             $path = str_replace("files/", "", $dir . "/" . $value);
             $url = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . $dir . "/" . $value;
-            if (strpos($path, "libraries") !== false) {
-               $type = "LIBRARY";
-            } else if (strpos($path, "mods") !== false) {
-               $type = "MOD";
-            } else if (strpos($path, "versions") !== false) {
-               if (substr($path, -5) == ".json") {
-                  $type = "VERIONSCUSTOM";
-               } else if(substr($path, -4) == ".jar"){
-                  $type = "VERIONS";
-               } else {
-                  $type = "FILE";
-               }
-            } else {
-               $type = "FILE";
-            }
-            echo "{\"path\":\"$path\",\"size\":$size,\"sha1\":\"$hash\",\"url\":\"$url\",\"type\":\"$type\"},";
+            
+            echo "{\"path\":\"$path\",\"size\":$size,\"sha1\":\"$hash\",\"url\":\"$url\"},";
          }
       }
    }
