cba999a4a6c0a677c34285ab42fa9cf2edaa18e0
Gabriel Dota <gabrieldotafujita1234@hotmail.com>
2023-04-18 14:28:55 -0300

    Refactor: Traduz algumas coisas para português

--- src/assets/js/panels/login.js
+++ src/assets/js/panels/login.js
@@ -194,7 +194,7 @@ class Login {
         let loginBtn = document.querySelector(".login-btn")
         let mojangBtn = document.querySelector('.mojang')
 
-        mojangBtn.innerHTML = "Offline"
+        mojangBtn.innerHTML = "Pirata"
 
         mojangBtn.addEventListener("click", () => {
             document.querySelector(".login-card").style.display = "none";
