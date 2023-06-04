15e95c6fa74645bc1350fb9678fc191f5e7797c8
Xertien <91416316+Xertien@users.noreply.github.com>
2023-06-04 18:41:52 +0200

    Update index.js

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,9 +22,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Ce launcher est trop opti", "author": "xertien" },
+            { "message": "Une pensée pour Babou", "author": "xertien" },
+            { "message": "Vive la B.A.S", "author": "xertien" }
         ]
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
