8353615436ea7778b9454b7dd0026caf996255ca
AuthenTiiq <47777491+AuthenTiiq@users.noreply.github.com>
2023-07-05 13:40:21 +0200

    Update

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,9 +22,10 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Je vois quelque chose en direction de.. RC", "author": "Explorateur" },+
+            { "message": "Et si ce n'était qu'un projet...", "author": "Explorateur" },
+            { "message": "Et si RoyalCreep's...", "author": "Inconnu" },
+            { "message": "RoyalCreep's est née en 2013...", "author": "Inconnu" }
         ]
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
