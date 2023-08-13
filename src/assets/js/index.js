051db9d2d14326866daf350682ecf38f30ee1c75
SaurFort <97303132+SaurFort@users.noreply.github.com>
2023-08-13 10:35:01 +0200

    SF-Update-3
    An update of the CSS (AGAIN)
    Change all loading phrase

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,9 +22,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Aly veut dominer le monde, attention", "author": "Saurfort" },
+            { "message": "Parce que c'est notre royaume !", "author": "Saurfort" },
+            { "message": "Le saviez-vous ? Je suis un launcher", "author": "Launcher" }
         ]
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
