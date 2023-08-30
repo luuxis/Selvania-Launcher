9664aa51847e7206cbebc1de4073da25b9d56480
Limeen <56938765+Limeen@users.noreply.github.com>
2023-08-30 17:59:21 +0200

    Update

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -83,8 +83,8 @@ class Splash {
     }
 
     shutdown(text) {
-        this.setStatus(`${text}<br>Arrêt dans 5s`);
-        let i = 4;
+        this.setStatus(`${text}<br>Arrêt dans 20s`);
+        let i = 19;
         setInterval(() => {
             this.setStatus(`${text}<br>Arrêt dans ${i--}s`);
             if (i < 0) ipcRenderer.send('update-window-close');
