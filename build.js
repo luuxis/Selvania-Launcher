3a7b82950c44970d3443f9129f5694b315e5a50e
Azakii1337 <84547694+AzakiiDev@users.noreply.github.com>
2023-09-19 01:48:07 +0200

    test
    test

--- build.js
+++ build.js
@@ -34,24 +34,6 @@ class Index {
                     createDesktopShortcut: true,
                     runAfterFinish: true
                 },
-                mac: {
-                    icon: "./src/assets/images/icon.icns",
-                    category: "public.app-category.games",
-                    target: [{
-                        target: "dmg",
-                        arch: ["x64", "arm64"]
-                    }]
-                },
-                linux: {
-                    icon: "./src/assets/images/icon.png",
-                    target: [{
-                        target: "AppImage",
-                        arch: ["x64"]
-                    }, {
-                        target: "tar.gz",
-                        arch: ["x64"]
-                    }]
-                }
             }
         }).then(() => {
             console.log('le build est terminé')
