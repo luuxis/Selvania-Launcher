bbb0fbdfc2dd2dcdf16b49535c2d83246e7f1eca
CHATDOO <143050541+CHATDOO@users.noreply.github.com>
2024-12-27 20:06:23 +0100

    V1

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Liberté égalité consanguinité. ", "author": "LE BUFFON" },
+            { "message": "Tu préfére moi ou ton père ?", "author": "Un Arabe" },
+            { "message": "Fumer tue , Péter tue.", "author": "CLODO" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
