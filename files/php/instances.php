418feb541b89d6530f7fd25ce3458d7b2c91a960
SROff23712 <sroff2371@gmail.com>
2025-07-07 22:39:56 +0200

    Update instances.php

--- files/php/instances.php
+++ files/php/instances.php
@@ -1,46 +1,18 @@
 <?php
-$instance['hypixel'] = array_merge($instance['hypixel'], array(
+$instance['SurvieGolmon'] = array_merge($instance['SurvieGolmon'], array(
     "loadder" => array(
-        "minecraft_version" => "1.8.9",
-        "loadder_type" => "forge",
+        "minecraft_version" => "1.21.5",
+        "loadder_type" => "NeoForge",
         "loadder_version" => "latest"
-    ),
-    "verify" => true,
-    "ignored" => array(
-        'config',
-        'essential',
-        'logs',
-        'resourcepacks',
-        'saves',
-        'screenshots',
-        'shaderpacks',
-        'W-OVERFLOW',
-        'options.txt',
-        'optionsof.txt'
-    ),
-    "whitelist" => array(),
-    "whitelistActive" => false,
-    "status" => array(
-        "nameServer" => "Hypixel",
-        "ip" => "mc.hypixel.net",
-        "port" => 25565
-    )
-));
-
-$instance['PokeMoonX'] = array_merge($instance['PokeMoonX'], array(
-    "loadder" => array(
-        "minecraft_version" => "1.16.5",
-        "loadder_type" => "forge",
-        "loadder_version" => "1.16.5-36.2.35"
     ),
     "verify" => false,
     "ignored" => array(),
     "whitelist" => array(),
     "whitelistActive" => false,
     "status" => array(
-        "nameServer" => "PokeMoonX",
-        "ip" => "178.32.106.234",
-        "port" => 25599
+        "nameServer" => "SurvieGolmon",
+        "ip" => "survieteamgolmon.falixsrv.me",
+        "port" => 22025
     )
 ));
 ?>
