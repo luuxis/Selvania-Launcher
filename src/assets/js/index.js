51bdab7a5d1a49b848052f1cea70d265089d11d5
Dorian Carriere <75777516+FurTorie@users.noreply.github.com>
2025-08-23 03:37:26 +0200

    Update index.js

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,12 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" },
-            { "message": "Haiko Launcher - Nouvelle génération !", "author": "DorianCarriere" },
-            { "message": "News épinglées, instances dynamiques... 🚀", "author": "DorianCarriere" },
-            { "message": "Interface moderne, expérience fluide ✨", "author": "DorianCarriere" }
+            { "message": "Haiko Launcher - Nouvelle génération !", "author": "DorianCarriere, Luuxis" },
+            { "message": "News épinglées, instances dynamiques🚀", "author": "DorianCarriere, Luuxis" },
+            { "message": "Interface moderne, expérience fluide✨", "author": "DorianCarriere, Luuxis" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
